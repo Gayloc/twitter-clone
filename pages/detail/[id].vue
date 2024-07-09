2db00225cb3882dcba146f48b878e76aad5d9328
Gayloc <gu18647403665@outlook.com>
2024-07-09 15:35:32 +0800

    feat: 新增加载动画

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -1,11 +1,12 @@
 <template>
     <v-container>
-        <v-row justify="center">
+        <v-row class="d-flex justify-center">
             <v-col cols="12" md="8">
-                <v-card>
+                <v-card v-if="tweet">
                     <v-card-title class="headline">{{ tweet.title }}</v-card-title>
                     <v-card-text>{{ tweet.content }}</v-card-text>
                 </v-card>
+                <v-alert v-else type="info">Loading...</v-alert>
             </v-col>
         </v-row>
     </v-container>
@@ -13,5 +14,5 @@
 
 <script setup>
 const route = useRoute()
-const { data: tweet } = await $fetch("/api/tweets/"+route.params.id)
+const { data: tweet } = await $fetch("/api/tweets/" + route.params.id)
 </script>
