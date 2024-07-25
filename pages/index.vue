777e9efbb76e2c8a29ba2a43ea2aca5bd16db79c
Edge <edge27452696803@gmail.com>
2024-07-26 00:54:04 +0800

    布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <v-container>
     <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Navigation drawer"/>
-      </v-list>
+      <v-list density="compact" nav>
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            subtitle="sandra_a88@gmailcom"
+            title="Me"
+          />
+        </v-list>
+        <v-divider/>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"/></v-list>
     </v-navigation-drawer>
     <v-main class="pa-0 ma-0">
       <v-row v-if="user" class="d-flex flex-wrap">
