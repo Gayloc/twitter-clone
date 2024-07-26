9118766455fefe16bf6b2cffe00e647e05a21459
Edge <edge27452696803@gmail.com>
2024-07-26 17:15:50 +0800

    小bug

--- middleware/login.global.ts
+++
@@ -1,9 +0,0 @@
-import { useUserStore } from '~/stores/user';
-
-export default defineNuxtRouteMiddleware((to) => {
-    const userStore = useUserStore();
-    if (to.path !== '/login' && to.path !== '/register' && to.path !== '/main' && to.path !== '/' && !userStore.Token) {
-        ElMessage.error('Please login first');
-        return navigateTo('/login');
-    }
-});
