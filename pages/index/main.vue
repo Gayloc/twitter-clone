5105fe968daece5866d0cb684b0e54b029016496
Edge <edge27452696803@gmail.com>
2024-07-27 23:17:08 +0800

    分页显示bug

--- pages/index/main.vue
+++ pages/index/main.vue
@@ -5,30 +5,31 @@
         md="12">
       <WelcomeCard/>
       </v-col>
-        <v-col 
+      <v-col 
         v-for="tweet in user.data" 
         :key="tweet.id"
         cols="12"
         md="4"
         >
-          <TweetCard :tweet="tweet" />
+        <TweetCard :tweet="tweet" />
+      </v-col>
+      <v-divider/>
+      <v-row class="d-flex justify-center">
+        <v-col cols="8" >
+          <v-container class="max-width ">
+            <v-pagination
+            v-model="page"
+            :length="length"
+            class="my-4"
+            />
+          </v-container>
         </v-col>
-        <v-row class="d-flex justify-center">
-          <v-col cols="8" >
-            <v-container class="max-width ">
-              <v-pagination
-                v-model="page"
-                :length="length"
-                class="my-4"
-              />
-            </v-container>
-          </v-col>
       </v-row>
     </v-row>
     <v-alert v-else type="info">{{ $t('loading') }}</v-alert>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 import TweetCard from '~/components/TweetCard.vue';
 import WelcomeCard from '~/components/WelcomeCard.vue';
 
