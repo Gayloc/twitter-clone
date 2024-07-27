339664c2ed24410ea8dcd716c75de3869cb7e00a
Edge <edge27452696803@gmail.com>
2024-07-27 20:30:17 +0800

    消息为空

--- pages/index/mylike.vue
+++ pages/index/mylike.vue
@@ -11,6 +11,19 @@
           <TweetCard 
             :tweet="tweet"/>
         </v-col>
+        <v-col 
+        v-if="userStore.user.userLikes.length === 0" 
+        cols="16" 
+        md="12" 
+        lg="6" 
+        style="min-width: 100%">
+        <v-empty-state
+            headline="No Messages Yet"
+            text="You haven't received any messages yet. When you do, they'll appear here."
+            title="Check back later."
+            @click:action="onClickAction"
+        />
+        </v-col>
     </v-row>
     <v-alert v-else type="info">{{ $t('loading') }}</v-alert>
 </template>
