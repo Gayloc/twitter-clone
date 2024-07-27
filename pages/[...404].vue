849f9c9657a46047801cd306f15af23757670314
Edge <edge27452696803@gmail.com>
2024-07-27 20:28:22 +0800

    404

---
+++ pages/[...404].vue
@@ -0,0 +1,8 @@
+<template>
+    <v-empty-state
+  headline="Whoops, 404"
+  title="Page not found"
+  text="The page you were looking for does not exist"
+  image="https://vuetifyjs.b-cdn.net/docs/images/logos/v.png"
+/>
+</template>
--- pages/index/myarticle.vue
+++ pages/index/myarticle.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        my article
-    </div>
+    <v-row class="d-flex justify-center">
+        <v-col cols="12">
+            <div style="min-height: 30%;">
+                <QuillEditor 
+                    v-model:content="content"
+                    theme="snow"
+                    style="min-height: 150px;"
+                />
+            </div>
+        </v-col>
+        <v-col cols="12" class="d-flex justify-end">
+            <v-btn color="primary" class="mr-4" @click="submit">Submit</v-btn>
+            <v-btn color="error" @click="reset">Reset</v-btn>
+        </v-col>
+    </v-row>
 </template>
+
+<script setup>
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+const content = '';
+</script>
