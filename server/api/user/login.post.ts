61b5df46ee12b2d10ed83b93c01373df96464b02
Edge <edge27452696803@gmail.com>
2024-07-30 21:00:04 +0800

    登录

--- server/api/user/login.post.ts
+++ server/api/user/login.post.ts
@@ -1,11 +1,13 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+// 请求体
 interface loginBody {
   email: string;
   password: string;
 }
 
+// 数据库中的用户信息
 interface user {
     id: number;
     email: string;
@@ -15,24 +17,39 @@ interface user {
     age: number;
 }
 
+// 数据库查询结果
 interface userRows { 
     rows: user[]; 
 }
 
+// 登录事件处理器
 const runtimeConfig = useRuntimeConfig();
+
+// JWT 密钥
 const JWT_SECRET = runtimeConfig.jwtSecret;
 
 export default defineEventHandler(async (event) => {
+    // 读取请求体
     const body = await readBody<loginBody>(event);
+
+    // 结构请求体
     const { email, password } = body;
+
+    // 密码邮箱不为空
     if (!email || !password) {
         throw createError({
             statusCode: 400,
             statusMessage: 'Email and password are required'
         });
     }
+
+    // 获取数据库连接
     const db = useDatabase();
+
+    // 查询用户
     const user = await db.sql<userRows>`SELECT * FROM users WHERE email = ${email};`;
+
+    // 用户不存在
     if (user.rows.length === 0) {
         throw createError({
             statusCode: 400,
@@ -40,6 +57,7 @@ export default defineEventHandler(async (event) => {
         });
     }
 
+    // 验证密码
     const foundUser = user.rows[0];
     const passwordMatch = await bcrypt.compare(password, foundUser.password);
 
@@ -50,6 +68,7 @@ export default defineEventHandler(async (event) => {
         });
     }
 
+    // 生成 JWT
     const token = jwt
         .sign(
             { userId: foundUser.id, userName: foundUser.user_name },
@@ -57,12 +76,14 @@ export default defineEventHandler(async (event) => {
             { expiresIn: '180d' }
     );
     
+    // 设置 cookie
     setCookie(event, 'auth_token', token, {
         httpOnly: true,
         secure: false,
         maxAge: 1000 * 60 * 60 * 24 * 180 // 180 days
     });
 
+    // 返回结果
     return {
         success: true,
         message: 'Login successful',
