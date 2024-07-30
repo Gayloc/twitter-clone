a667a4525cb449e2ccc3b2d72e82204b063ee2fd
Edge <edge27452696803@gmail.com>
2024-07-30 20:58:16 +0800

    delete

--- server/api/user/delete.delete.ts
+++ server/api/user/delete.delete.ts
@@ -1,10 +1,12 @@
 import authMiddle from '~/server/utils/auth';
 import bcrypt from 'bcrypt';
 
+// 删除用户请求体
 interface deleteBody {
     password: string;
 }
 
+// 用户信息
 interface user {
     id: number;
     email: string;
@@ -14,32 +16,48 @@ interface user {
     age: number;
 }
 
+// 用户信息列表
 interface userRows { 
     rows: user[]; 
 }
 export default defineEventHandler(async (event) => {
+    // TODO 验证用户
     await authMiddle(event);
+
+    // TODO 获取请求体
     const body = await readBody<deleteBody>(event);
     const { password } = body;
 
+    // 获取用户信息
     const userInfo = event.context.auth;
+
+    // 获取数据库连接
     const db = useDatabase();
     
     try {
+        // TODO 密码不能为空
         if (!password) {
             throw createError({
                 statusCode: 400,
                 statusMessage: 'password are required'
             });
         }
+
+        // TODO 获取用户信息
         const user = await db.sql<userRows>`SELECT * FROM users WHERE id = ${userInfo.userId};`;
+
+        // TODO 用户不存在
         if (user.rows.length === 0) {
             throw createError({
                 statusCode: 400,
                 statusMessage: 'user not found'
             });
         }
+
+        // 用户信息
         const foundUser = user.rows[0];
+
+        // TODO 验证密码
         const passwordMatch = await bcrypt.compare(password, foundUser.password);
         if (!passwordMatch) {
             throw createError({
@@ -47,6 +65,8 @@ export default defineEventHandler(async (event) => {
                 statusMessage: 'password is incorrect'
             });
         }
+
+        // TODO 删除用户
         if (passwordMatch) {
             db.sql`DELETE FROM users WHERE id = ${foundUser.id};`;
             return {
@@ -55,6 +75,7 @@ export default defineEventHandler(async (event) => {
             };
         }
     } catch (error) {
+        // TODO 处理错误
         console.error(error);
         throw error;
     }
