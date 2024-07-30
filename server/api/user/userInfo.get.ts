b3a4367f945de4c989915c738426ee36eb9df4ac
Edge <edge27452696803@gmail.com>
2024-07-30 21:06:22 +0800

    info

--- server/api/user/userInfo.get.ts
+++ server/api/user/userInfo.get.ts
@@ -1,5 +1,6 @@
 import authMiddle from '~/server/utils/auth';
 
+// 用户信息
 interface user {
     id: number;
     email: string;
@@ -9,39 +10,56 @@ interface user {
     age: number;
 }
 
+// 用户信息列表
 interface userRows { 
     rows: user[]; 
 }
 
+// 用户喜欢数据格式
 interface userLike {
     user_id: number;
     like_id: number;
 }
 
+// 用户喜欢列表
 interface userLikeList {
     rows: userLike[];
 }
 
 export default defineEventHandler(async (event) => { 
+    // 验证用户是否登录
     await authMiddle(event);
 
+    // 获取用户信息
     const userInfo = event.context.auth;
+
+    // 获取数据库连接
     const db = useDatabase();
 
     try {
+
+        // 获取用户信息
         const { rows: userInfos } =
             await db.sql<userRows>`SELECT * FROM users WHERE id = ${userInfo.userId}`;
+        
+        // 用户不存在
         if (userInfos === undefined) {
             throw new Error('User not found');
         }
+
+        // 用户不存在
         if (userInfos.length === 0) {
             return {
                 success: false,
                 message: 'User not found'
             };
         }
+
+        // 获取用户喜欢列表
         const { rows: likes } =
             await db.sql<userLikeList>`SELECT * FROM user_like WHERE user_id = ${userInfo.userId}`;
+        
+        // 返回用户信息
         return {
             success: true,
             userIn: {
