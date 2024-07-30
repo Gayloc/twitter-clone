12713563915a001489f8c39e45393a5ee152caf9
Edge <edge27452696803@gmail.com>
2024-07-30 21:00:52 +0800

    登出

--- server/api/user/logout.get.ts
+++ server/api/user/logout.get.ts
@@ -1,9 +1,12 @@
 export default defineEventHandler(async (event) => {
+    // 清除 cookie
     setCookie(event, 'auth_token', '', {
         httpOnly: true,
         secure: false,
         maxAge: -1
     });
+
+    // 返回结果
     return {
         statusCode: 200,
         success: true,
