e8733c380944df2c731cacaac4cdc81687a86fee
Edge <edge27452696803@gmail.com>
2024-07-30 21:03:43 +0800

    注册

--- server/api/user/register.post.ts
+++ server/api/user/register.post.ts
@@ -1,10 +1,12 @@
 import bcrypt from 'bcrypt';
 
+// 注册请求体
 interface registerBody {
     email: string,
     password: string,
 }
 
+// 用户信息
 interface user {
     id: number;
     email: string;
@@ -14,14 +16,17 @@ interface user {
     age: number;
 }
 
+// 用户信息数组
 interface userRows { 
     rows: user[]; 
 }
 
 export default defineEventHandler(async (event) => { 
+    // 读取请求体
     const body = await readBody<registerBody>(event);
     const { email, password } = body;
 
+    // 验证邮箱密码是否为空
     if (!email || !password) {
         throw createError({
             statusCode: 400,
@@ -29,11 +34,14 @@ export default defineEventHandler(async (event) => {
         });
     }
 
+    // 获取数据库连接
     const db = useDatabase();
 
+    // 检查邮箱是否已注册
     const existingUser = 
         await db.sql<userRows>`SELECT * FROM users WHERE email = ${email}`;
     
+    // 如果已注册，抛出错误
     if (existingUser.rows.length > 0) {
         throw createError({
             statusCode: 400,
@@ -41,9 +49,11 @@ export default defineEventHandler(async (event) => {
         });
     }
 
+    // 加密密码
     const hashedPassword = await bcrypt.hash(password, 10);
     await db.sql`INSERT INTO users (email, user_name, password) VALUES (${email}, ${email}, ${hashedPassword})`;
 
+    // 返回成功信息
     return {
         success: true,
         message: 'User created successfully'
