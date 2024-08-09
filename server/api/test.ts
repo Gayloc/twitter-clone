e3684d02e46781928bfc2084604c2431ceac225c
Edge <edge27452696803@gmail.com>
2024-08-09 13:37:38 +0800

    添加测试内容

--- server/api/test.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async () => {
-    const db = useDatabase();
-    // Query for users
-    const { rows } = await db.sql`SELECT * FROM users`;
-    return {
-      rows
-    };
-  });
